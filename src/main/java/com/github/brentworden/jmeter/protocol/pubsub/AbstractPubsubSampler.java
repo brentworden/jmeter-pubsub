e92a754b85429c6a34258db5bdc55e43de73396c
Brent Worden <brent.worden@bestbuy.com>
2018-10-20 12:58:13 -0500

    initial project structure

---
+++ src/main/java/com/github/brentworden/jmeter/protocol/pubsub/AbstractPubsubSampler.java
@@ -0,0 +1,9 @@
+package com.github.brentworden.jmeter.protocol.pubsub;
+
+import org.apache.jmeter.samplers.AbstractSampler;
+
+public abstract class AbstractPubsubSampler extends AbstractSampler {
+
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/github/brentworden/jmeter/protocol/pubsub/PubsubPublisherSampler.java
@@ -0,0 +1,15 @@
+package com.github.brentworden.jmeter.protocol.pubsub;
+
+import org.apache.jmeter.samplers.Entry;
+import org.apache.jmeter.samplers.SampleResult;
+
+public class PubsubPublisherSampler extends AbstractPubsubSampler {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public SampleResult sample(Entry e) {
+        return null;
+    }
+
+}
